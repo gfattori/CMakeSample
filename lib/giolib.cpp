ae7c35768ff180e0e4234680b3b9b2e211512dfd
Giovanni Fattori <g.fattori@gmail.com>
2012-01-15 23:44:47 +0100

    colorfull text

--- lib/giolib.cpp
+++ lib/giolib.cpp
@@ -1,7 +1,18 @@
 #include "giolib.h"
+#include <iostream>
+using namespace std;
 
 void giolib ()
 {
-printf("giolib helloworld");
+printf("giolib helloworld\n");
+//  std::cout << "\33[0;31m" << "test" << "\33[0m" << std::endl ;
+
+
+for (int i = 31; i <= 37; i++)
+{
+std::cout << "\033[0;" << i << "mHello!\033[0m" << std::endl;
+std::cout << "\033[1;" << i << "mHello!\033[0m" << std::endl;
+}
+
 }
 
